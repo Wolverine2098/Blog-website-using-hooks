1ddbec3e26e164f45abb93ac80b217655c768739
Raunak <raunak.goswami@indiamart.com>
2022-06-26 02:05:54 +0530

    initialising directory structure

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+
+
+
+function App() {
+  return (
+    <div className="App">
+      Starting React Vlog
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+  const firebaseConfig = {
+    apiKey: "AIzaSyCgraDp7iyYWW4MrRYM4QoOXji3Zp1BOHA",
+    authDomain: "react-hooks-blog-5136f.firebaseapp.com",
+    projectId: "react-hooks-blog-5136f",
+    storageBucket: "react-hooks-blog-5136f.appspot.com",
+    messagingSenderId: "767995400684",
+    appId: "1:767995400684:web:1d101ef7c78c82dae770c9"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+export const firestore = firebase.firestore();
