a9688d19ede4bd07681e466b6e5f3eb0ebbe2e46
Raunak <raunak.goswami@indiamart.com>
2022-06-27 03:24:27 +0530

    adding hooks to check form states

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -1,5 +1,43 @@
+// import { useState } from "react/cjs/react.production.min";
+import react from "react";
+
+import { useState } from "react" 
 function CreatePost() {
-  return <div className="">CreatePost</div>;
+  const [title,setTitle] = useState('');
+  const [subTitle,setSubTitle] = useState('');
+  const [content,setContent] = useState('');
+
+function handleSubmit(e){
+  e.preventDefault();
+  console.log('title',title);
+  console.log('Subtitle',subTitle);
+  console.log('content',content);
+}
+
+  return <div className="create-post">
+    <h1>Create post</h1>
+    <form onSubmit= {handleSubmit}>
+
+      <div className ="form-field">
+        <label>Title</label>
+        <input value={title} onChange={ (e) => setTitle(e.target.value)}/>
+</div>
+
+        <div className ="form-field">
+        <label>Subtitle</label>
+        <input value={subTitle} onChange={ (e) => setSubTitle(e.target.value)}/>
+        </div>
+
+
+        <div className ="form-field">
+        <label>Content</label>
+        <textarea value={content} onChange={(e)=>setContent(e.target.value)}></textarea>
+
+      </div>
+      <button className = "create-post-btn">Create Post</button>
+    </form>
+
+    </div>
 }
 
 export default CreatePost;
