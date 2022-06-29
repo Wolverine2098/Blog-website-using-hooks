f16b4c980fd2ad236de656e50b83da6e2db4d069
Raunak <raunak.goswami@indiamart.com>
2022-06-30 02:20:54 +0530

    adding css using styled components

--- src/components/PostDetail.js
+++ src/components/PostDetail.js
@@ -1,6 +1,7 @@
 import { firestore } from '../firebase';
 import {useState,useEffect} from 'react';
 import {useParams} from 'react-router-dom';
+import Radium from 'radium';
 function PostDetail() {
 
 const [post,setPost] = useState({});
@@ -21,9 +22,25 @@ setPost(snapshot.data());
 },[])
   return <div className="post-detail">
 
-    <h1>{post.title}</h1>
-    <p>{post.content}</p>
+    <h1 style = {styles.heading}>{post.title}</h1>
+    <p style = {styles.postDetail} >{post.content}</p>
   </div>;
 }
 
-export default PostDetail;
+export default Radium(PostDetail);
+
+const styles = {
+   
+  postDetail:{
+    border: '1px solid #e1e1e1',
+    padding :5,
+    paddingTop :10,
+    borderRadius : 5, 
+  },
+
+  heading: {
+    margin:0,
+    textAlign: 'Center',
+    color:'blue',
+  },
+}
